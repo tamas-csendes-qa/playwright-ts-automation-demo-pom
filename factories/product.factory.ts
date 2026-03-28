69856af0e8f40bd07264a12fbd1c210562726ccb
Tomi <Tomi@DESKTOP-8F1FU4F>
2026-03-28 18:36:41 +0100

    user, product factories

--- factories/product.factory.ts
+++ factories/product.factory.ts
@@ -6,4 +6,20 @@ export const ProductFactory = {
   sauceLabsBackpack(overrides?: Partial<Product>): Product {
     return { name: 'Sauce Labs Backpack', ...overrides };
   },
+
+  sauceLabsBikeLight(overrides?: Partial<Product>): Product {
+    return { name: 'Sauce Labs Bike Light', ...overrides };
+  },
+
+  sauceLabsBoltTShirt(overrides?: Partial<Product>): Product {
+    return { name: 'Sauce Labs Bolt T-Shirt', ...overrides };
+  },
+
+  sauceLabsFleeceJacket(overrides?: Partial<Product>): Product {
+    return { name: 'Sauce Labs Fleece Jacket', ...overrides };
+  },
+
+  sauceLabsOnesie(overrides?: Partial<Product>): Product {
+    return { name: 'Sauce Labs Onesie', ...overrides };
+  },
 };
--- factories/user.factory.ts
+++ factories/user.factory.ts
@@ -7,4 +7,24 @@ export const UserFactory = {
   standardUser(overrides?: Partial<User>): User {
     return { username: 'standard_user', password: 'secret_sauce', ...overrides };
   },
+
+  lockedOutUser(overrides?: Partial<User>): User {
+    return { username: 'locked_out_user', password: 'secret_sauce', ...overrides };
+  },
+
+  problemUser(overrides?: Partial<User>): User {
+    return { username: 'problem_user', password: 'secret_sauce', ...overrides };
+  },
+
+  performanceGlitchUser(overrides?: Partial<User>): User {
+    return { username: 'performance_glitch_user', password: 'secret_sauce', ...overrides };
+  },
+
+  errorUser(overrides?: Partial<User>): User {
+    return { username: 'error_user', password: 'secret_sauce', ...overrides };
+  },
+
+  visualUser(overrides?: Partial<User>): User {
+    return { username: 'visual_user', password: 'secret_sauce', ...overrides };
+  },
 };
