1af22d7058501709e6a11dee52441d6015f38e4c
Tomi <Tomi@DESKTOP-8F1FU4F>
2026-04-09 21:46:51 +0200

    fix

--- factories/user.factory.ts
+++ factories/user.factory.ts
@@ -5,26 +5,50 @@ export type User = {
 
 export const UserFactory = {
   standardUser(overrides?: Partial<User>): User {
-    return { username: 'standard_user', password: 'secret_sauce', ...overrides };
+    return {
+      username: 'standard_user',
+      password: 'secret_sauce',
+      ...overrides,
+    };
   },
 
   lockedOutUser(overrides?: Partial<User>): User {
-    return { username: 'locked_out_user', password: 'secret_sauce', ...overrides };
+    return {
+      username: 'locked_out_user',
+      password: 'secret_sauce',
+      ...overrides,
+    };
   },
 
   problemUser(overrides?: Partial<User>): User {
-    return { username: 'problem_user', password: 'secret_sauce', ...overrides };
+    return {
+      username: 'problem_user',
+      password: 'secret_sauce',
+      ...overrides,
+    };
   },
 
   performanceGlitchUser(overrides?: Partial<User>): User {
-    return { username: 'performance_glitch_user', password: 'secret_sauce', ...overrides };
+    return {
+      username: 'performance_glitch_user',
+      password: 'secret_sauce',
+      ...overrides,
+    };
   },
 
   errorUser(overrides?: Partial<User>): User {
-    return { username: 'error_user', password: 'secret_sauce', ...overrides };
+    return {
+      username: 'error_user',
+      password: 'secret_sauce',
+      ...overrides,
+    };
   },
 
   visualUser(overrides?: Partial<User>): User {
-    return { username: 'visual_user', password: 'secret_sauce', ...overrides };
+    return {
+      username: 'visual_user',
+      password: 'secret_sauce',
+      ...overrides,
+    };
   },
 };
